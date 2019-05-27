62e458cc0c2ae49b93612514c40060465c439b71
jiwan tamang <jiwan.tamang255@gmail.com>
2019-05-27 08:02:36 +0545

    sdfs

--- control/UnEquipItemCommand.java
+++ control/UnEquipItemCommand.java
@@ -6,7 +6,6 @@ public class UnEquipItemCommand implements Command{
 	private CommandResponse response;
 	@Override
 	public CommandResponse execute(ParsedInput userInput, Player thePlayer) {
-		// TODO Auto-generated method stub
 		this.response = new CommandResponse("syntax error: unequip-item");
 		
 		if(userInput.getArguments().size() == 0){
